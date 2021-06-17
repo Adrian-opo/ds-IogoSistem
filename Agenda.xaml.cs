8edd710952c89fe3fa696646cddb475fe55e70b1
Wwester <westerjesuino@gmail.com>
2021-06-17 13:08:32 -0400

    Menu_teste e Tela agenda

---
+++ Agenda.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IogoSistem_vs1
+{
+    /// <summary>
+    /// Lógica interna para Agenda.xaml
+    /// </summary>
+    public partial class Agenda : Window
+    {
+
+        public Agenda()
+        {
+            InitializeComponent();
+            Loaded += Agenda_Loaded;
+            
+        }
+
+        private void Agenda_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Evento> listaAgenda = new List<Evento>();
+            for(int i=1; i<=24; i++)
+            {
+                listaAgenda.Add(new Evento()
+                {
+                    Horas = i+":00",
+                    Domingo = "X",
+                    Segunda = "X",
+                    Terca = "X",
+                    Quarta = "X",
+                    Quinta = "X",
+                    Sexta = "X",
+                    Sabado = "X"
+                    
+                });
+            }
+
+
+
+            //dataGridAgenda.ItemsSource = listaAgenda; 
+                
+        }
+
+    }
+}
---
+++ Menu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IogoSistem_vs1
+{
+    /// <summary>
+    /// Lógica interna para Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCadastrarUsuario(object sender, RoutedEventArgs e)
+        {
+            MainWindow abrir = new MainWindow();
+            abrir.ShowDialog();
+        }
+
+        private void BtnAgenda(object sender, RoutedEventArgs e)
+        {
+            Agenda agenda = new Agenda();
+            agenda.ShowDialog();
+        }
+    }
+}
